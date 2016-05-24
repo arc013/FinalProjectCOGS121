8b482b2409fd43e2a4972a79bb4bb243304b2de9
Alisa Prathnadi <aprathna@ucsd.edu>
2016-05-23 19:55:00 -0700

    the map and the menu

--- finalProject/app.js
+++ finalProject/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +13,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('html', handlebars());
+app.set("view engine", "html");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,6 +25,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/menu', routes);
+app.use('/maptest', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- finalProject/routes/index.js
+++ finalProject/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/menu', function(req, res, next) {
+  res.render('menu');
+});
+
+router.get('/maptest', function(req, res, next) {
+  res.render('maptest');
+});
+
 module.exports = router;
